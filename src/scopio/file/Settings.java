0dd4b8b141b2955520ee54d5d53ccb7295e07702
Lithax <Lithax@outlook.de>
2024-11-18 16:45:03 +0100

    tweaked settings

--- src/scopio/file/Settings.java
+++ src/scopio/file/Settings.java
@@ -1,11 +1,18 @@
 package scopio.file;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class Settings extends FileProcessor {
+    private HashMap<String, HashMap<String, String>> settings = new HashMap<>();
+
     public Settings() {
         super(new File("../../config/.settings"));
+        String raw = read();
+        String[] split = raw.split("[$]");
+        for(String settingGroup : split) {
+            String header = settingGroup.substring(0, settingGroup.indexOf('{'));
+            settings.put(header, new HashMap<>());
+        }
     }
-
-    
 }
